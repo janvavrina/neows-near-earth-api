886abd005a9d928e5af3a2b3b09a06d943ca5847
Jan Vavřina <JAN.VAVRINA@seznam.cz>
2023-03-04 17:58:28 +0100

    Base external request working

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from typing import Union
 
 from fastapi import FastAPI
+import requests
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
+BASE_URL = "https://api.nasa.gov/neo/rest/v1"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+
+@app.get("/objects")
+def read_nasa(start_date: str, end_date: str):
+    payload = {"start_date": start_date, "end_date": end_date, "api_key": API_KEY}
+    response = requests.get(f"{BASE_URL}/feed", params=payload).json()
+    return response
