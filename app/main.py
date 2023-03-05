1a6cede7fd61704d641850f1127d3eef3f0efbf2
Jan Vavřina <JAN.VAVRINA@seznam.cz>
2023-03-05 19:08:00 +0100

    Return JSON array of filtered JSON objects

--- app/main.py
+++ app/main.py
@@ -1,18 +1,51 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
 import requests
 
 from pydantic import BaseModel
 
+from app import settings
+
 app = FastAPI()
 
-BASE_URL = "https://api.nasa.gov/neo/rest/v1"
 
+# class RangeIn(BaseModel):
+#     start_date: str
+#     end_date: str
+#     api_key: str = API_KEY
+
+
+# class ObjectsOut(BaseModel):
 
 
 @app.get("/objects")
-def read_nasa(start_date: str, end_date: str):
-    payload = {"start_date": start_date, "end_date": end_date, "api_key": API_KEY}
-    response = requests.get(f"{BASE_URL}/feed", params=payload).json()
-    return response
+def read_nasa(start_date: str | None = Query(default="2022-11-09",
+                                             min_length=10,
+                                             max_length=10,
+                                             regex=settings.DATE_FORMAT_REGEX),
+              end_date: str | None = Query(default="2022-11-11",
+                                           min_length=10,
+                                           max_length=10,
+                                           regex=settings.DATE_FORMAT_REGEX)
+              ):
+    payload = {"start_date": start_date, "end_date": end_date, "api_key": settings.API_KEY}
+    response = requests.get(f"{settings.BASE_URL}/feed", params=payload).json()
+    dict_res = dict(response)
+    try:
+        near_earth_objects = dict_res["near_earth_objects"]
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Item not found")
+    if near_earth_objects:
+        items = near_earth_objects.items()
+
+        items = [
+            {
+                key: value
+                for attribute in attributes
+                for key, value in attribute.items()
+                if key in {'name', 'estimated_diameter', 'close_approach_data'}
+            }
+            for date, attributes in items
+        ]
+    return items
---
+++ app/settings.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://api.nasa.gov/neo/rest/v1"
+API_KEY = "0aY3SLEaTKPLFThhX7cpAnQeQ9dSXGLgTfGryBLD"
+DATE_FORMAT_REGEX = "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"
