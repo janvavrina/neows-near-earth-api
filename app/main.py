241a863e1943d988d923235e015fac99db985500
Jan Vavřina <JAN.VAVRINA@seznam.cz>
2023-03-06 01:03:14 +0100

    Added sorting & check for format of dates

--- app/main.py
+++ app/main.py
@@ -2,24 +2,23 @@ from typing import Union
 
 from fastapi import FastAPI, Query, HTTPException
 import requests
-
-from pydantic import BaseModel
+import re
 
 from app import settings
 
 app = FastAPI()
 
 
-# class RangeIn(BaseModel):
-#     start_date: str
-#     end_date: str
-#     api_key: str = API_KEY
+def bool_match_regex(text: str, pattern: str):
+    return bool(re.match(text, pattern))
 
 
-# class ObjectsOut(BaseModel):
+@app.get("/", status_code=200)
+def hello():
+    return "hello The MAMA AI"
 
 
-@app.get("/objects")
+@app.get("/objects", status_code=200)
 def read_nasa(start_date: str | None = Query(default="2022-11-09",
                                              min_length=10,
                                              max_length=10,
@@ -29,16 +28,19 @@ def read_nasa(start_date: str | None = Query(default="2022-11-09",
                                            max_length=10,
                                            regex=settings.DATE_FORMAT_REGEX)
               ):
+    if bool_match_regex(start_date, settings.DATE_FORMAT_REGEX) \
+            or bool_match_regex(end_date, settings.DATE_FORMAT_REGEX):
+        raise HTTPException(status_code=403, detail="Incorrect format of dates")
+
     payload = {"start_date": start_date, "end_date": end_date, "api_key": settings.API_KEY}
     response = requests.get(f"{settings.BASE_URL}/feed", params=payload).json()
-    dict_res = dict(response)
+    # dict_res = dict(response)
     try:
-        near_earth_objects = dict_res["near_earth_objects"]
+        near_earth_objects = response["near_earth_objects"]
     except KeyError:
         raise HTTPException(status_code=404, detail="Item not found")
     if near_earth_objects:
         items = near_earth_objects.items()
-        # TODO sort by closest approach
         items = [
             {
                 key: value
@@ -48,4 +50,8 @@ def read_nasa(start_date: str | None = Query(default="2022-11-09",
             }
             for date, attributes in items
         ]
+        items.sort(key=lambda x: (x.get('close_approach_data')[0]
+                                  .get('miss_distance')
+                                  .get('astronomical')),
+                   reverse=False)
     return items
