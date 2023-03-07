b4c2bfc0a73d982ec75bc86ca562d7a0979de665
Jan Vavřina <JAN.VAVRINA@seznam.cz>
2023-03-07 13:32:24 +0100

    Regex fixes

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 
 def bool_match_regex(text: str, pattern: str):
-    return bool(re.match(text, pattern))
+    return bool(re.match(pattern, text))
 
 
 @app.get("/", status_code=200)
@@ -36,44 +36,41 @@ def read_nasa(start_date: str | None = Query(default="2022-11-09",
     :param end_date: string specifying ending date in YYYY-MM-DD format
     :return: array of  JSON objects containing name, estimated diameter, close_approach_data
     """
-    if bool_match_regex(start_date, settings.DATE_FORMAT_REGEX) \
-            or bool_match_regex(end_date, settings.DATE_FORMAT_REGEX):
-        raise HTTPException(status_code=403, detail="Incorrect format of dates")
+    if not bool_match_regex(start_date, settings.DATE_FORMAT_REGEX) \
+            or not bool_match_regex(end_date, settings.DATE_FORMAT_REGEX):
+        raise HTTPException(status_code=400, detail="Incorrect format of dates")
 
     payload = {"start_date": start_date, "end_date": end_date, "api_key": settings.API_KEY}
     response = requests.get(f"{settings.BASE_URL}/feed", params=payload).json()
-
-    if not response:
-        raise HTTPException(status_code=503, detail="External API not available")
 
     try:
         near_earth_objects = response["near_earth_objects"]
     except KeyError:
         raise HTTPException(status_code=404, detail="Item not found")
-    if near_earth_objects:
-        neo_items = near_earth_objects.items()
 
-        # creates array of objects
-        neo_items = [
-            {
-                **{
-                    key: value[0] if isinstance(value, list) and len(value) == 1 else value
-                    for key, value in neo.items()
-                    if key in settings.LF_ATTRIBUTES
-                },
-                'date': date  # adds date to other keys
-            }
-            for date, neos in neo_items
-            for neo in neos
-        ]
+    neo_items = near_earth_objects.items()
 
-        # sort items by distance
-        try:
-            def key_distance(x):
-                return x['close_approach_data']['miss_distance']['astronomical']
+    # creates array of objects
+    neo_items = [
+        {
+            **{
+                key: value[0] if isinstance(value, list) and len(value) == 1 else value
+                for key, value in neo.items()
+                if key in settings.LF_ATTRIBUTES
+            },
+            'date': date  # adds date to other keys
+        }
+        for date, neos in neo_items
+        for neo in neos
+    ]
 
-            neo_items.sort(key=key_distance, reverse=False)
-        except KeyError:
-            raise HTTPException(status_code=404, detail="Item not found")
+    # sort items by distance
+    try:
+        def key_distance(x):
+            return x['close_approach_data']['miss_distance']['astronomical']
+
+        neo_items.sort(key=key_distance, reverse=False)
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Item not found")
 
     return neo_items
