fc2568cac326769e258855c9bf9b2af4b1c9aa67
Jan Vavřina <JAN.VAVRINA@seznam.cz>
2023-03-10 19:08:03 +0100

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseSettings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    app_name: str = "API"
+
+    BASE_URL = "https://api.nasa.gov/neo/rest/v1"
+    DATE_FORMAT_REGEX = "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"
+    LF_ATTRIBUTES = {'name', 'estimated_diameter', 'close_approach_data'}
+
+    API_KEY = os.environ.get('API_KEY')
+
+
+settings = Settings()
